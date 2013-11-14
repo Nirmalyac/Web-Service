69daef47a791f3184def9d93a62a1be555a38e74
Nirmalyac <Chakraborty.Nirmalya@gmail.com>
2013-11-14 23:34:44 +0530

    Initial Creation

---
+++ SOAP-webservice/Consumers/Unmanaged/WSClientRPCLIT/src/main/java/com/nc/ws/cxf/Client.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.nc.ws.cxf;
+
+/**
+ * @author ibm
+ *
+ */
+public class Client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
